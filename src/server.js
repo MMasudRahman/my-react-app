b82be31f5e738d47483a77a065ded706a0040213
MD Masudur Rahman <u783206@footlocker.com>
2021-03-19 14:58:09 -0400

    authentication to Azure Active Directory instance

--- src/server.js
+++ src/server.js
@@ -4,6 +4,10 @@ import { MongoClient } from 'mongodb';
 import path from 'path';
 import fs from 'fs';
 import https from 'https';
+require('dotenv').config();
+import cors from 'cors';
+import jwt from 'jsonwebtoken';
+import utils from './utils';
 
 [
     {
@@ -23,7 +27,7 @@ import https from 'https';
 
 const app = express();
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 const options = {
     key: fs.readFileSync(path.join(__dirname, '/certs/server-key.pem'), 'utf8'),
@@ -32,7 +36,13 @@ const options = {
 
 
 app.use(express.static(path.join(__dirname, '/build')));
+// enable CORS
+app.use(cors());
+// parse application/json
 app.use(bodyParser.json());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const withDB = async (operations, res) => {
     try {
@@ -47,8 +57,100 @@ const withDB = async (operations, res) => {
     }
 }
 
+//middleware that checks if JWT token exists and verifies it if it does exist.
+//In all future routes, this helps to know if the request is authenticated or not.
+app.use(function (req, res, next) {
+    // check header or url parameters or post parameters for token
+    var token = req.headers['authorization'];
+    if (!token) return next(); //if no token, continue
+  
+    token = token.replace('Bearer ', '');
+    jwt.verify(token, process.env.JWT_SECRET, function (err, user) {
+      if (err) {
+        return res.status(401).json({
+          error: true,
+          message: "Invalid user."
+        });
+      } else {
+        req.user = user; //set the user to req so other routes can use it
+        next();
+      }
+    });
+  });
 
-     
+// request handlers
+app.get('/', (req, res) => {
+    if (!req.user) return res.status(401).json({ success: false, message: 'Invalid user to access it.' });
+    res.send('Welcome to the FLI React App! - ' + req.user.name);
+  });
+
+// validate the user credentials
+app.post('/users/signin', function (req, res) {
+    const user = req.body.username;
+    const pwd = req.body.password;
+  
+    // return 400 status if username/password is not exist
+    if (!user || !pwd) {
+      return res.status(400).json({
+        error: true,
+        message: "Username or Password required."
+      });
+    }
+  
+    const [userData, setUserData] = useState({userId: "", password: "", name: "", username: "" });
+    useEffect(() => {
+    withDB(async (db) => {
+      const uData = await db.collection('users').findOne({ username: user,password: pwd }).json();
+      setUserData(uData);
+  }, res);
+}, []);
+    // return 401 status if the credential is not match.
+    if (user !== userData.username || pwd !== userData.password) {
+      return res.status(401).json({
+        error: true,
+        message: "Username or Password is Wrong."
+      });
+    }
+  
+    // generate token
+    const token = utils.generateToken(userData);
+    // get basic user details
+    const userObj = utils.getCleanUser(userData);
+    // return the token along with user details
+    return res.json({ user: userObj, token });
+  });
+
+// verify the token and return it if it's valid
+app.get('/verifyToken', function (req, res) {
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.query.token;
+    if (!token) {
+      return res.status(400).json({
+        error: true,
+        message: "Token is required."
+      });
+    }
+    // check token that was passed by decoding token using secret
+    jwt.verify(token, process.env.JWT_SECRET, function (err, user) {
+      if (err) return res.status(401).json({
+        error: true,
+        message: "Invalid token."
+      });
+  
+      // return 401 status if the userId does not match.
+      if (user.userId !== userData.userId) {
+        return res.status(401).json({
+          error: true,
+          message: "Invalid user."
+        });
+      }
+      // get basic user details
+      var userObj = utils.getCleanUser(userData);
+      return res.json({ user: userObj, token });
+    });
+  });
+  
+
 app.get('/api/articles', async (req, res) => {
     withDB(async (db) => {
     
